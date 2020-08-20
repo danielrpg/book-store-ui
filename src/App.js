3b5ef52613b3146bf6a20443429fa8bc50e98f04
daniel fernandez <daniel.fernandezgp@gmail.com>
2020-08-20 15:48:43 -0400

    First changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Grid,
+  Header,
+  Image,
+  Segment,
+  Form,
+  Button,
+  Message,
+} from "semantic-ui-react";
+import logo from "./images/logo.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h2" color="teal" textAlign="center">
+        <Image src={logo} /> Book Store
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="E-mail address"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+            type="password"
+          />
+
+          <Button type="submit" color="teal" fluid size="large">
+            Login
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        New to us? <a href="/sigin">Sign Up</a>
+      </Message>
+    </Grid.Column>
+  </Grid>
+);
 
 export default App;
